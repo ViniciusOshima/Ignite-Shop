af509499c832bf60cc1319148ff7d353c3934c9d
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-08-09 19:38:28 -0300

    FEAT link do next para acessar a page products, sua estruturação e estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { HomeContainer, Product } from '@/styles/pages/home'
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { useKeenSlider } from 'keen-slider/react'
-
 import 'keen-slider/keen-slider.min.css'
-import { GetStaticProps } from 'next'
+
+import { HomeContainer, Product } from '@/styles/pages/home'
+
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
 
@@ -29,14 +31,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className='keen-slider__slide'>
-            <Image src={product.imageUrl} width={520} height={480} alt='' />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={480} alt='' />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
