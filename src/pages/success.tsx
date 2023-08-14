dbd76b55fe80cc15473b54e8a1a853d2e3251d1d
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-08-14 19:42:16 -0300

    FEAT estruturação e estilização da page Success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,21 @@
+import { SuccesContainer, ImageContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccesContainer>
+      <h1>Compra Efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Vini Oshima</strong>, sua <strong>Camiseta Beyond the limits</strong> já está a caminho de sua casa.
+      </p>
+
+      <Link href='/'>
+        Voltar ao catálogo</Link>
+    </SuccesContainer>
   )
 }
